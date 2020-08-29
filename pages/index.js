b1a6a7d208cfe49da8c3f2cb403b12f42798691a
Eshaan Mathur <eshaan.mathur@gmail.com>
2020-08-29 22:35:34 +0530

    organised imports

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,5 @@
-import { useState } from 'react';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-
 import Layout from '../components/Layout';
 import PokemanModal from '../components/PokemanModal';
 
